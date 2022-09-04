321441bd19e32b80b91da596a64a9dbc1102cb38
dru <dru@gamebreakingstudios.com>
2022-09-04 12:44:56 -0700

    feat: walking monk exists

---
+++ assets/sprites/monk/monk.rs
@@ -0,0 +1,26 @@
+//
+//  Sprite sheet data for Amethyst Game Engine: https://www.amethyst.rs
+//  This file was created by TexturePacker: https://www.codeandweb.com/texturepacker
+//
+//  $TexturePacker:SmartUpdate:758f76345a7c1ff0540b0bb12e109fb7:8a6c4d4be66109a24dcab33551ad20ae:24368fe6b4d2e434ae606b8d3a2d3275$
+//
+
+#![allow(dead_code)]
+
+pub const WALK_DOWN_U_MONK_WEST_WALKA_X1_121: usize = 0;
+pub const WALK_DOWN_U_MONK_WEST_WALKA_X1_130: usize = 1;
+pub const WALK_DOWN_U_MONK_WEST_WALKA_X1_140: usize = 2;
+pub const WALK_DOWN_U_MONK_WEST_WALKA_X1_150: usize = 3;
+pub const WALK_LEFT_U_MONK_WEST_WALKA_X1_240: usize = 4;
+pub const WALK_LEFT_U_MONK_WEST_WALKA_X1_250: usize = 5;
+pub const WALK_LEFT_U_MONK_WEST_WALKA_X1_260: usize = 6;
+pub const WALK_LEFT_U_MONK_WEST_WALKA_X1_270: usize = 7;
+pub const WALK_RIGHT_U_MONK_WEST_WALKA_X1_001: usize = 8;
+pub const WALK_RIGHT_U_MONK_WEST_WALKA_X1_010: usize = 9;
+pub const WALK_RIGHT_U_MONK_WEST_WALKA_X1_020: usize = 10;
+pub const WALK_RIGHT_U_MONK_WEST_WALKA_X1_030: usize = 11;
+pub const WALK_UP_U_MONK_WEST_WALKA_X1_361: usize = 12;
+pub const WALK_UP_U_MONK_WEST_WALKA_X1_370: usize = 13;
+pub const WALK_UP_U_MONK_WEST_WALKA_X1_380: usize = 14;
+pub const WALK_UP_U_MONK_WEST_WALKA_X1_390: usize = 15;
+
